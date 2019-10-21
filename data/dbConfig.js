c571e36d613a0a42deea19cc381da60d49da9f30
Nicole Beamer <tulip@Nicoles-iMac.home.sagemod>
2019-10-21 16:00:11 -0600

    fixing deployed environment

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,15 +1,15 @@
-// const knex = require('knex');
-// const environment = process.env.NODE_ENV || 'development';
-
-
-
-// const knexConfig = require('../knexfile.js');
-
-
-// module.exports = knex(knexConfig.development)[environment];
-
 const knex = require('knex');
+const environment = process.env.NODE_ENV || 'development';
+
+
 
 const knexConfig = require('../knexfile.js');
 
-module.exports = knex(knexConfig.development);
+
+module.exports = knex(knexConfig.development)[environment];
+
+// const knex = require('knex');
+
+// const knexConfig = require('../knexfile.js');
+
+// module.exports = knex(knexConfig.development);
