57135bc13d7918bd48e446025f54056985854a66
Nicole Beamer <tulip@Nicoles-iMac.home.sagemod>
2019-10-21 17:08:44 -0600

    fixed sqlite error 1 , table not exist, incorrect db file referenced in production knex

--- knexfile.js
+++ knexfile.js
@@ -19,30 +19,30 @@ module.exports = {
       directory: "./data/seeds"
     }
   },
-  // production: {
-  //   client: "sqlite3",
-  //   useNullAsDefault: true,
-  //   connection: {
-  //     filename: "./data/blog.db3"
-  //   },
-  //   pool: {
-  //     afterCreate: (conn, done) => {
-  //       conn.run("PRAGMA foreign_keys = ON", done);
-  //     }
-  //   },
-  //   migrations: {
-  //     directory: "./data/migrations",
-  //     tableName: "knex_migrations"
-  //   },
-  //   seeds: {
-  //     directory: "./data/seeds"
-  //   }
-  // }
   production: {
-    client: "postgres",
-    connection: process.env.DATABASE_URL,
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/dbHacker.db3"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
     migrations: {
-      directory: "/data/migrations"
+      directory: "./data/migrations",
+      tableName: "knex_migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
+  // production: {
+  //   client: "postgres",
+  //   connection: process.env.DATABASE_URL,
+  //   migrations: {
+  //     directory: "/data/migrations"
+  //   }
+  // }
 };
