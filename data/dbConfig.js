273057f01a2279138e2076cd72570a0d6ca9d5ac
Nicole Beamer <tulip@Nicoles-iMac.home.sagemod>
2019-10-21 21:11:17 -0600

    seems to be a dbconfig issue with heroku logs...not clear yet if really the error

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,8 +1,9 @@
 const knex = require('knex');
 const environment = process.env.NODE_ENV || 'development';
 const knexConfig = require('../knexfile.js');
+const knexObj = knexConfig[environment];
 
-module.exports = knex(knexConfig[environment]);
+module.exports = knex(knexObj);
 
 // const knex = require('knex');
 
